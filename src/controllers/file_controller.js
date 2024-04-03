b0398140868e31ef1db92cdc69c4097152f7bfb7
HoangLong501 <101213673+HoangLong501@users.noreply.github.com>
2024-04-03 16:14:35 +0700

    Update file_controller.js

--- src/controllers/file_controller.js
+++ src/controllers/file_controller.js
@@ -1,5 +1,7 @@
 // const FileExcel = require('../models/FileExcel');
 const XLSX = require('xlsx');
+const SinhVien = require('../models/sinhvien');
+
 
 const FileExcelController ={
    uploadFile(req ,res){
@@ -7,8 +9,16 @@ const FileExcelController ={
   const sheetName = workbook.SheetNames[0];
   const sheet = workbook.Sheets[sheetName];
   const jsonData = XLSX.utils.sheet_to_json(sheet);
+  //console.log(jsonData);
   res.json(jsonData);
+
+  for(let i=0;i<jsonData.length;i++){
+   SinhVien.insertSinhVien(jsonData[i]);
+   //console.log(jsonData[i]);
+  }
+
    }
+   
 }
 
 module.exports=FileExcelController;
